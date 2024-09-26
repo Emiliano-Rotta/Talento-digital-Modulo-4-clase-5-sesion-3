65bc23d9ab63ffd5b2a253a6a244b3014bf074de
Emiliano Rotta <emilianorottaf@gmail.com>
2024-09-25 21:39:42 -0300

    ejercicios

---
+++ assets/js/sesion4.js
@@ -0,0 +1,21 @@
+// const persona = {
+//     nombre: "Carlos",
+//     edad:30,
+//     ciudad: "Santiago"
+// }
+
+// //antes de ES6
+// // const nombre = persona.nombre
+// // const edad = persona.edad
+// // const ciudad = persona.ciudad
+
+// //con ES6
+// const { nombre, edad, ciudad } = persona
+
+// console.log(nombre, edad, ciudad)
+// //---------------------
+// const frutas = ["banana","manzana", "melón", "sandia"]
+
+// const [frutaA, frutaB,, frutaC] = frutas
+
+// console.log(frutaA, frutaB, frutaC)
